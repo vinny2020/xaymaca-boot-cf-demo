056659762038e8b467be1e1ad57e2da6ec5d4028
Vincent Stoessel <backbase@Vinny-MBP.local>
2016-08-25 22:17:11 -0400

    init commit, now including code

---
+++ src/main/java/com/xaymaca/boot/web/HelloMotoController.java
@@ -0,0 +1,18 @@
+package com.xaymaca.boot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Vincent Stoessel on August 25, 2016.
+ */
+@Controller
+public class HelloMotoController {
+
+    @RequestMapping("/hello")
+    public String helloW(Model model) {
+        model.addAttribute("name", "Moto");
+        return "hello";
+    }
+}
