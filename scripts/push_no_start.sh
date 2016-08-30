7b01e0b2f184c5b9ce4e24f9a969563a7575303c
Vincent Stoessel <backbase@Vinny-MBP.local>
2016-08-30 15:14:14 -0400

    swapping out shells

--- scripts/push_no_start.sh
+++ scripts/push_no_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 echo "this script is executing"
 echo "cf is :" `which cf`
-#cf push --no-start  -p target/xaymaca-boot-cf-demo.jar
+#cf push --no-start  -p ./target/xaymaca-boot-cf-demo.jar
